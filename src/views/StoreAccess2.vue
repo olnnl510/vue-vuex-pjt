f8341a53070984d33de19418aa33de0a7cb30147
YoonJeongIn <101864135+olnnl510@users.noreply.github.com>
2022-07-22 17:42:42 +0900

    first commit

---
+++ src/views/StoreAccess2.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Count : {{ count }} </p>
+    <button type="button" @click="increment">증가</button>
+  </div>
+</template>
+
+<script>
+export default {
+computed: {
+    count() {
+        return this.$store.state.count;
+    }
+},
+methods: {
+    increment() {
+        this.$store.commit('increment');
+        console.log(1);
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
